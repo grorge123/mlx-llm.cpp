13117f12eb2ac9d111de6c40f38377370da99f9c
grorge <g97116@yahoo.com>
2024-08-13 13:06:50 +0800

    fix: change static_cast to dynamic_cast

--- model/transformer.cpp
+++ model/transformer.cpp
@@ -18,62 +18,62 @@ Attention::forward(mx::array Input, std::optional<mx::array> Mask,
   const auto &[B, L, D] =
       std::tie(Input.shape()[0], Input.shape()[1], Input.shape()[2]);
   mx::array Queries =
-      static_cast<nn::Linear *>(Submodules["q_proj"])->forward(Input);
+      dynamic_cast<nn::Linear *>(Submodules["q_proj"])->forward(Input);
   mx::array Keys =
-      static_cast<nn::Linear *>(Submodules["k_proj"])->forward(Input);
+      dynamic_cast<nn::Linear *>(Submodules["k_proj"])->forward(Input);
   mx::array Values =
-      static_cast<nn::Linear *>(Submodules["v_proj"])->forward(Input);
+      dynamic_cast<nn::Linear *>(Submodules["v_proj"])->forward(Input);
   Queries = transpose(reshape(Queries, {B, L, NHeads, -1}), {0, 2, 1, 3});
   Keys = transpose(reshape(Keys, {B, L, NKVHeads, -1}), {0, 2, 1, 3});
   Values = transpose(reshape(Values, {B, L, NKVHeads, -1}), {0, 2, 1, 3});
 
   if (NormQKProj) {
-    Queries = static_cast<RMSNorm *>(Submodules["q_norm"])->forward(Queries);
-    Keys = static_cast<RMSNorm *>(Submodules["k_norm"])->forward(Keys);
+    Queries = dynamic_cast<RMSNorm *>(Submodules["q_norm"])->forward(Queries);
+    Keys = dynamic_cast<RMSNorm *>(Submodules["k_norm"])->forward(Keys);
   }
 
   if (!KVCache) {
     const auto &[KeyCache, ValueCache] = *KVCache;
-    Queries = static_cast<nn::RoPE *>(Submodules["nn::RoPE"])
+    Queries = dynamic_cast<nn::RoPE *>(Submodules["nn::RoPE"])
                   ->forward(Queries, KeyCache.shape(2));
-    Keys = static_cast<nn::RoPE *>(Submodules["nn::RoPE"])
+    Keys = dynamic_cast<nn::RoPE *>(Submodules["nn::RoPE"])
                ->forward(Keys, KeyCache.shape(2));
     Keys = mx::concatenate({KeyCache, Keys}, 2);
     Values = mx::concatenate({KeyCache, Keys}, 2);
   } else {
-    Queries = static_cast<nn::RoPE *>(Submodules["nn::RoPE"])->forward(Queries);
-    Keys = static_cast<nn::RoPE *>(Submodules["nn::RoPE"])->forward(Keys);
+    Queries = dynamic_cast<nn::RoPE *>(Submodules["nn::RoPE"])->forward(Queries);
+    Keys = dynamic_cast<nn::RoPE *>(Submodules["nn::RoPE"])->forward(Keys);
   }
   mx::array Output = mx::fast::scaled_dot_product_attention(
       Queries, Keys, Values, Scale, Mask);
   Output = reshape(transpose(Output, {0, 2, 1, 3}), {B, L, -1});
-  return {static_cast<nn::Linear *>(Submodules["o_proj"])->forward(Output),
+  return {dynamic_cast<nn::Linear *>(Submodules["o_proj"])->forward(Output),
           {Keys, Values}};
 }
 mx::array MLP::forward(mx::array Input) {
   if (Gemma) {
-    return static_cast<nn::Linear *>(Submodules["down_proj"])
+    return dynamic_cast<nn::Linear *>(Submodules["down_proj"])
         ->forward(gelu(
-            static_cast<nn::Linear *>(Submodules["gate_proj"])->forward(Input) *
-            static_cast<nn::Linear *>(Submodules["up_proj"])->forward(Input)));
+            dynamic_cast<nn::Linear *>(Submodules["gate_proj"])->forward(Input) *
+            dynamic_cast<nn::Linear *>(Submodules["up_proj"])->forward(Input)));
   }
-  return static_cast<nn::Linear *>(Submodules["down_proj"])
+  return dynamic_cast<nn::Linear *>(Submodules["down_proj"])
       ->forward(silu(
-          static_cast<nn::Linear *>(Submodules["gate_proj"])->forward(Input) *
-          static_cast<nn::Linear *>(Submodules["up_proj"])->forward(Input)));
+          dynamic_cast<nn::Linear *>(Submodules["gate_proj"])->forward(Input) *
+          dynamic_cast<nn::Linear *>(Submodules["up_proj"])->forward(Input)));
 }
 std::tuple<mx::array, std::tuple<mx::array, mx::array>>
 TransformerBlock::forward(
     mx::array Input, std::optional<mx::array> Mask,
     std::optional<std::tuple<mx::array, mx::array>> KVCachePar) {
   auto [R, KVCache] =
-      static_cast<Attention *>(Submodules["attention"])
-          ->forward(static_cast<RMSNorm *>(Submodules["attention_norm"])
+      dynamic_cast<Attention *>(Submodules["attention"])
+          ->forward(dynamic_cast<RMSNorm *>(Submodules["attention_norm"])
                         ->forward(Input),
                     Mask, KVCachePar);
   auto H = Input + R;
-  R = static_cast<MLP *>(Submodules["mlp"])
-          ->forward(static_cast<RMSNorm *>(Submodules["mlp_norm"])->forward(H));
+  R = dynamic_cast<MLP *>(Submodules["mlp"])
+          ->forward(dynamic_cast<RMSNorm *>(Submodules["mlp_norm"])->forward(H));
   return {H + R, KVCache};
 }
 std::tuple<mx::array,
@@ -83,7 +83,7 @@ Transformer::embed(
     std::optional<std::vector<std::tuple<mx::array, mx::array>>> KVCachePar,
     bool Norm) {
   auto H =
-      static_cast<mx::nn::Embedding *>(Submodules["embed"])->forward(Input);
+      dynamic_cast<mx::nn::Embedding *>(Submodules["embed"])->forward(Input);
   if (Gemma) {
     H = H * (pow(Dim, 0.5));
   }
@@ -109,9 +109,9 @@ Transformer::embed(
   }
   if (Norm) {
     if (!Gemma) {
-      return {static_cast<nn::RMSNorm *>(Submodules["norm"])->forward(H), {}};
+      return {dynamic_cast<nn::RMSNorm *>(Submodules["norm"])->forward(H), {}};
     }
-    return {static_cast<RMSNorm *>(Submodules["norm"])->forward(H), {}};
+    return {dynamic_cast<RMSNorm *>(Submodules["norm"])->forward(H), {}};
   }
   return {H, KVCache};
 }
@@ -123,10 +123,10 @@ Transformer::forward(
   auto [X, KVCache] = embed(Input, KVCachePar);
   mx::array Out = {};
   if (EmbedAsHead) {
-    Out = static_cast<mx::nn::Embedding *>(Submodules["token_embed"])
+    Out = dynamic_cast<mx::nn::Embedding *>(Submodules["token_embed"])
               ->asLinear(X);
   } else {
-    Out = static_cast<nn::Linear *>(Submodules["head"])->forward(X);
+    Out = dynamic_cast<nn::Linear *>(Submodules["head"])->forward(X);
   }
   return {Out, KVCache};
 }
