07686afa0d0ccb3e7bf768c9b75ea1d12388ef9e
grorge <g97116@yahoo.com>
2024-08-16 22:23:59 +0800

    feat: add llama2, llama3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "base.h"
 #include "mlx/mlx.h"
 #include "model/converter.h"
+#include "model/registry.h"
 #include "model/transformer.h"
 #include "prompt/llama.h"
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <mlx/array.h>
+#include <mlx/device.h>
 #include <mlx/dtype.h>
 #include <string>
 #include <tokenizers_cpp.h>
@@ -27,54 +29,42 @@ std::string LoadBytesFromFile(const std::string &path) {
   fs.read(data.data(), size);
   return data;
 }
-enum AnserSataus{
+enum AnserSataus {
   STOP,
   WAIT,
   GO,
 };
-AnserSataus answerSataus(std::string Text, std::string End){
-  if(Text.ends_with(End)){
+AnserSataus answerSataus(std::string Text, std::string End) {
+  if (Text.ends_with(End)) {
     return STOP;
   }
-  for(int Idx = 1 ; Idx < End.size() ; Idx++){
-    if(Text.ends_with(End.substr(0, Idx))){
+  for (int Idx = 1; Idx < End.size(); Idx++) {
+    if (Text.ends_with(End.substr(0, Idx))) {
       return WAIT;
     }
   }
   return GO;
 }
 int main() {
+  std::cout << mx::default_device() << " " << mx::metal::is_available()
+            << std::endl;
+  mx::set_default_device(mx::Device::gpu);
   auto Tok = Tokenizer::FromBlobJSON(LoadBytesFromFile("../tokenizer.json"));
-  // const std::string Prompt = "What is the capital of Canada?";
   const int MaxToken = 512;
-  // const std::vector<int> Ids = Tok->Encode(Prompt);
-  // const mx::array Token =
-  //     mx::array(Ids.data(), {static_cast<int>(Ids.size())}, mx::int32);
   mx::array Token = mx::array({{1, 23, 35, 48, 87, 62}, {6}});
   std::cout << "Create Model...\n";
-  // llama2-7b
-  //  const int VocabSize = 32000;
-  //  const float NormEps = 1e-5;
-  //  const float RopeTheta = 10000.0;
-  //  const bool RopeTraditional = false;
-  //  auto Model = Transformer(4096, std::vector<int>{11008}, VocabSize, 32,
-  //                           std::vector<int>{32}, std::vector<int>{32},
-  //                           NormEps, RopeTheta, RopeTraditional);
   const int VocabSize = 32000;
   const float NormEps = 1e-5;
   const float RopeTheta = 10000.0;
   const bool RopeTraditional = false;
-  auto Model = Transformer(2048, std::vector<int>{5632}, VocabSize, 22,
-                           std::vector<int>{32}, std::vector<int>{4}, NormEps,
-                           {}, RopeTraditional, RopeTheta);
+  auto Model = llama38b();
   std::cout << "Load Model...\n";
   // Model.update(llamaToMlxllm("../llama2-7b"));
-  Model.update(llamaToMlxllm("../tiny"));
+  Model.update(llamaToMlxllm("../llama3-8b"));
   std::cout << "Start generate...\n";
   const TinyLLaMAPrompt Prmopt;
-  const std::vector<int> Ids = Tok->Encode("How are you?");
-  Token =
-      mx::array(Ids.data(), {static_cast<int>(Ids.size())}, mx::int32);
+  const std::vector<int> Ids = Tok->Encode("Where are you from?");
+  Token = mx::array(Ids.data(), {static_cast<int>(Ids.size())}, mx::int32);
   std::vector<int32_t> TokenList;
   std::string Answer;
   int Skip = 0;
@@ -82,7 +72,7 @@ int main() {
   auto [Y, KVCache] = Model.generate(Token, 0.1);
   while (true) {
     TokenCount++;
-    if(TokenCount > MaxToken){
+    if (TokenCount > MaxToken) {
       break;
     }
     eval(Y);
@@ -95,10 +85,10 @@ int main() {
     TokenList.insert(TokenList.end(), Tokens.begin(), Tokens.end());
     Answer = Tok->Decode(TokenList);
     const AnserSataus Status = answerSataus(Answer, Prmopt.TextEnd);
-    if(Status == STOP){
+    if (Status == STOP) {
       break;
     }
-    if(Status == GO){
+    if (Status == GO) {
       std::cout << Answer.substr(Skip) << std::flush;
       Skip = Answer.size();
     }
